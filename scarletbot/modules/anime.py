702d14bf3b3a703b84382107725e15cdae508789
Kunal <88786146+iamproaf@users.noreply.github.com>
2023-04-10 17:19:01 +0530

    Update anime.py

--- scarletbot/modules/anime.py
+++ scarletbot/modules/anime.py
@@ -139,7 +139,7 @@ dispatcher.add_handler(QUOTE_CHANGE)
 dispatcher.add_handler(ANIMEQUOTES_HANDLER)
 dispatcher.add_handler(QUOTES_HANDLER)
 
-__mod_name__ = "♨️ǫᴜᴏᴛᴇs♨️"
+__mod_name__ = "『ǫᴜᴏᴛᴇS 』"
 __help__ = """
 
 /quote :- ᴡʀɪᴛᴇ ǫᴜᴏᴛᴇs
