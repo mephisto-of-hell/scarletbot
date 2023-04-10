d1627ebe03afec6400f36b1f17e806599bc73408
Kunal <88786146+iamproaf@users.noreply.github.com>
2023-04-11 05:15:39 +0530

    Update currency_converter.py

--- scarletbot/modules/currency_converter.py
+++ scarletbot/modules/currency_converter.py
@@ -57,7 +57,7 @@ dispatcher.add_handler(CONVERTER_HANDLER)
 
 __command_list__ = ["cash"]
 __handlers__ = [CONVERTER_HANDLER]
-__mod_name__ = "♨️ᴄᴀsʜ♨️"
+__mod_name__ = "『ᴄᴀsʜ 』"
 __help__ = """
  /ᴄᴀsʜ  ᴄᴏɴᴠᴇʀᴛ ᴇᴀᴄʜ ᴏᴛʜᴇʀ ᴄᴜʀʀᴇɴᴄʏ 
  /ᴄᴏɴᴠᴇʀᴛ ᴏʀ ᴛʀʏ 
