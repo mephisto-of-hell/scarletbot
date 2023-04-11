2998a714e7752a4037da22d1eb7c59afc1ffe89b
Kunal <88786146+iamproaf@users.noreply.github.com>
2023-04-11 07:59:05 +0530

    Update logo.py

--- scarletbot/modules/logo.py
+++ scarletbot/modules/logo.py
@@ -319,7 +319,7 @@ async def lego(event):
             os.remove(fname)
 
 
-__mod_name__ = "📍ʟᴏɢᴏ📍"
+__mod_name__ = "『Lᴏɢᴏ』"
 
 __help__ = """
 I can create some beautiful and attractive logo for your profile pics.
